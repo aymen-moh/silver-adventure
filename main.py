f519065b7ff80f6f9c10acd38f0692f0c4aee9dc
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-11-03 21:02:39 +0300

    started on the player

--- main.py
+++ main.py
@@ -8,29 +8,80 @@ pygame.init()
 
 pygame.display.set_caption("Game")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 2000, 1200
 FPS = 60
 PLAYER_VEL = 5 
 
-window = pygame.display.set_mode(WIDTH, HEIGHT)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+class Player(pygame.sprite.sprite):
+  COLOR = (255, 0, 0)
+  
+  def __init__(self, x, y, width, height):
+    self.rect = pygame.Rect(x, y, width, height)
+    self.x_vel = 0 
+    self.y_vel = 0 
+    self.mask = None
+    self.direction = "left"
+    self.animation_count = 0
+  def move(self, dx, dy):
+    self.rect.x += dx
+    self.rect.y += dy 
+  
+  def move_left(self, vel):
+    self.x_vel = -vel
+    if self.direction != "left":
+      self.direction = "left"
+      self.animation_count = 0 
+  
+  def move_right(self, vel)
+  self.x_vel = vel
+  if self.direction != "right":
+    self.direction = "right"
+    self.animation_count = 60
+    
+  def loop(self, fps):
+    self.move(self.x_vel, self.y_vel)
+    
+  def draw(self, win):
+    pygame.draw.rect(win, self.COLOR, self.rect)
 
 def get_background(name):
-  image = pygame.image.load(join("assets", "Background", name))
-  _, _, width, height = image.get_rect()
-def main(window):
-  clock = pygame.time.clock()
-  run = True
-  while run:
-    clock.tick(FPS)
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
     
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          run = False
-          break
-  pygame.quit()
-  quit()
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+        
+    return tiles, image
+ # i didntmknow how to draw the bg and i got syntax error
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+    pygame.display.update()
+  
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Green.png")
+    
+    player = Player(100,100, 50, 50)
+    
+    run = True
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw(window, background, bg_image)
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
-  main(window)
+    main(window)
