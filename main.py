87a2a4de22cb31ff088ac1ea5b3f66ec024a7f39
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-11-04 16:27:32 +0300

    added animations, learnt alotgit add .

--- main.py
+++ main.py
@@ -8,15 +8,42 @@ pygame.init()
 
 pygame.display.set_caption("Game")
 
-WIDTH, HEIGHT = 2000, 1200
+WIDTH, HEIGHT = 600, 600
 FPS = 60
 PLAYER_VEL = 5 
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+  return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+  
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    all_sprites = {}
 
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+    
 class Player(pygame.sprite.Sprite):
   COLOR = (255, 0, 0)
+  GRAVITY = 0 # i TEMPORARLY changed this to 0 so i can test animations lol
+  SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+  animd = 5
   
   def __init__(self, x, y, width, height):
     self.rect = pygame.Rect(x, y, width, height)
@@ -25,6 +52,8 @@ class Player(pygame.sprite.Sprite):
     self.mask = None
     self.direction = "left"
     self.animation_count = 0
+    self.fall_count = 0
+    
   def move(self, dx, dy):
     self.rect.x += dx
     self.rect.y += dy 
@@ -42,10 +71,27 @@ class Player(pygame.sprite.Sprite):
       self.animation_count = 60
     
   def loop(self, fps):
+    self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
     self.move(self.x_vel, self.y_vel)
     
+    self.fall_count += 1
+    self.update_sprite()
+  def update_sprite(self):
+    if self.x_vel != 0:
+        sprite_sheet = "run"
+    else:
+        sprite_sheet = "idle"
+
+    sprite_sheet_name = sprite_sheet + "_" + self.direction
+    sprites = self.SPRITES[sprite_sheet_name]
+    sprite_index = (self.animation_count // self.animd) % len(sprites)
+    self.sprite = sprites[sprite_index]
+    self.animation_count += 1
+    
+    
+    
   def draw(self, win):
-    pygame.draw.rect(win, self.COLOR, self.rect)
+    win.blit(self.sprite, (self.rect.x, self.rect.y))
     
 
 def get_background(name):
@@ -65,7 +111,16 @@ def draw(window, background, bg_image, player):
         window.blit(bg_image, tile)
     player.draw(window)
     pygame.display.update()
+def handle_move(player):
+  keys = pygame.key.get_pressed()
   
+  
+  if keys[pygame.K_a]:
+    player.move_left(PLAYER_VEL)
+  if keys[pygame.K_d]:
+    player.move_right(PLAYER_VEL)
+    
+    
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Green.png")
@@ -80,6 +135,11 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        player.loop(FPS)
+        handle_move(player)
+        
+        
+        
         draw(window, background, bg_image, player)
     pygame.quit()
     quit()
